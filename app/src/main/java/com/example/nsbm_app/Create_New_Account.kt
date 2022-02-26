f0567815c62b7bd3b2de034e978705ce45ace014
Nck Arachchci <76784581+nckarachchi@users.noreply.github.com>
2022-02-27 04:27:16 +0530

    hello word

---
+++ app/src/main/java/com/example/nsbm_app/Create_New_Account.kt
@@ -0,0 +1,11 @@
+package com.example.nsbm_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Create_New_Account : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_new_account)
+    }
+}
